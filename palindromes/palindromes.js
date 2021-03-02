edebaaba114a5d3362dc63561ed719cfb6989cf5
pumaisub <65642598+pumaisub@users.noreply.github.com>
2021-03-02 15:33:20 -0800

    Add files via upload
    my palindromes function is inside the HTML file, not the JS file

---
+++ palindromes/palindromes.js
@@ -0,0 +1,19 @@
+const palindromes = function(stringCheck) {
+    if (typeof stringCheck == "string"){
+        stringCheck.split("");
+        console.table(stringCheck);
+    } else {
+
+    }
+}
+
+module.exports = palindromes
+
+
+
+
+//take in a string and check to see if the reverse is equal to the original 
+//but remove punctuation, exclamation points and spaces ,, also account for capital letters 
+
+
+
